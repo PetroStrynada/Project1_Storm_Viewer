7c3c37ea4ef6eaa42d37fa114e553f505eceb14b
Petro Strynada <petro.strynada@gmail.com>
2023-06-12 17:15:14 +0300

    Completed video tutorial + challenge
    https://www.hackingwithswift.com/100

---
+++ Project1_Storm_Viewer/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetaleViewController.swift
+//  Project1_Storm_Viewer
+//
+//  Created by Petro Strynada on 02.06.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedImageNumber: Int?
+    var imageCount: Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Picture \(selectedImageNumber ?? 0) of \(imageCount ?? 0)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project1_Storm_Viewer
 The 100 Days of Swift
+
+You’ll meet: 
+view controllers, storyboards, and the FileManager class
+UIImageView, UIImage, UINavigationBar, and more.
+
+Day 16...18
+https://www.hackingwithswift.com/100
